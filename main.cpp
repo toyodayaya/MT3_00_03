16c79061c791aa0c5b113ce72f368f55cc54a95d
toyodayaya <k024g0031@g.neec.ac.jp>
2025-04-18 10:44:37 +0900

    00_03の確認課題

--- main.cpp
+++ main.cpp
@@ -143,7 +143,6 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale)
 	m.m[1][0] = 0.0f; m.m[1][1] = scale.y; m.m[1][2] = 0.0f; m.m[1][3] = 0.0f;
 	m.m[2][0] = 0.0f; m.m[2][1] = 0.0f; m.m[2][2] = scale.z; m.m[2][3] = 0.0f;
 	m.m[3][0] = 0.0f; m.m[3][1] = 0.0f; m.m[3][2] = 0.0f; m.m[3][3] = 1.0f;
-
 	return m;
 }
 
